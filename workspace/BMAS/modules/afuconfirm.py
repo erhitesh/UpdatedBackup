7252c189464ab279fd755b63447dcd3f70607b2f
erhitesh <bhardwajhitesh2@gmail.com>
2016-12-13 10:19:12 +0530

    asndai

--- workspace/BMAS/modules/afuconfirm.py
+++
@@ -1,9 +0,0 @@
-from realm.helpers import wait_text
-
-def afu_confirm_pd(test):
-    """ Steps to confirm eCard is send by afu account. 
-        @param test: Instance of test-case.
-        
-    """  
-    wait_text(test,"Your eCard has been sent!")
-  
--- workspace/BMAS/test.py
+++
@@ -1,5 +0,0 @@
-import unittest2 as unittest
-
-if __name__ == "__main__":
-    suite = unittest.TestLoader().discover('./tests/Tst', pattern = "*test.py")
-    unittest.TextTestRunner(verbosity=2).run(suite)
